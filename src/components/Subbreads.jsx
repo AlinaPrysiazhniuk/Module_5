18ca87643193efec4b18e09d473f33b03c827517
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-02 18:14:54 +0200

    subbreads

--- src/components/Subbreads.jsx
+++ src/components/Subbreads.jsx
@@ -2,5 +2,10 @@ import { useParams } from 'react-router-dom';
 
 export const Subbreads = () => {
   const { dogId } = useParams();
+
+  // useEffect(() => {
+  //   http запит
+  // })
+
   return <div>Subbreads: {dogId}</div>;
 };
