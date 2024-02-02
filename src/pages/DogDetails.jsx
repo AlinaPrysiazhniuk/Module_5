901642f955a18d9d62b1e990beb1e04b25089bda
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-02 16:27:14 +0200

    dogDetails

--- src/pages/DogDetails.jsx
+++ src/pages/DogDetails.jsx
@@ -1,11 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 const DogDetails = () => {
   const { dogId } = useParams();
   //useEffect(() => {
   //Http запит
   //}, [])
-  return <>DogDetails: {dogId}</>;
+  return (
+    <>
+      <h1>DogDetails: {dogId}</h1>
+      <ul>
+        <li>
+          <Link to="subbreads">Під породи</Link>
+        </li>
+        <li>
+          <Link to="gallery">Галерея</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </>
+  );
 };
 
 export default DogDetails;
